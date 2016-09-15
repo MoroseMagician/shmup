0cd47dd89cb602e2548a9be0998a2192f0307d6a
morosemagician <odinn08@gmail.com>
2016-09-15 20:03:05 +0000

    Refactoring and some small stuff Created the basic framework for the shoot mechanic. Lots more to do there

--- src/main.py
+++ src/main.py
@@ -1,41 +1,47 @@
 import os
 import sys
+import ship
 import pygame
 import load_assets
 
 pygame.init()
-size = width, height = 1280, 720
+size = width, height = 224 * 3, 288 * 3
 screen = pygame.display.set_mode(size)
 
-ship = load_assets.load_image("ship.png")
-ship_rect = ship.get_rect()
+ship = ship.Ship(width, height, 3)
 
 timer = pygame.time.Clock()
 
+def keyCheck(ship, keys, mods, speed = 2):
+    
+    if mods & pygame.KMOD_LSHIFT:
+        speed = 1
+
+    if keys[pygame.K_UP] and ship.rect.y > 0:
+        ship.move_up(speed)
+    if keys[pygame.K_DOWN] and ship.rect.y + ship.rect.height < height:
+        ship.move_down(speed)
+    if keys[pygame.K_LEFT] and ship.rect.x > 0:
+        ship.move_left(speed)
+    if keys[pygame.K_RIGHT] and ship.rect.x + ship.rect.width < width:
+        ship.move_right(speed)
+
+    if keys[pygame.K_z]:
+        ship.shoot()
+    
 while True:
     
-    speed = 4
     keys = pygame.key.get_pressed()
-    modifiers = pygame.key.get_mods()
+    mods = pygame.key.get_mods()
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
-    if modifiers & pygame.KMOD_LSHIFT:
-        speed = 2
-    
-    if keys[pygame.K_UP] and ship_rect[1] > 0:
-        ship_rect = ship_rect.move(0,-speed)
-    if keys[pygame.K_DOWN] and ship_rect[1] + ship_rect[2] < height:
-        ship_rect = ship_rect.move(0,speed)
-    if keys[pygame.K_LEFT] and ship_rect[0] > 0:
-        ship_rect = ship_rect.move(-speed,0)
-    if keys[pygame.K_RIGHT] and ship_rect[0] + ship_rect[3] < width:
-        ship_rect = ship_rect.move(speed,0)
+    keyCheck(ship, keys, mods)
 
     screen.fill([0,0,0])
-    screen.blit(ship, ship_rect)
+    screen.blit(ship.image, ship.rect)
     pygame.display.update()
 
     timer.tick(60)
--- src/ship.py
+++ src/ship.py
@@ -1,10 +1,31 @@
 from load_assets import load_image
+import bullet
 
 class Ship:
-    def __init__(self):
+    def __init__(self, screenwidth, screenheight, speed):
         self.image = load_image("ship.png")
         self.rect = self.image.get_rect()
-        self.height = 
-        self.width = 
-        self.position = 
-        self.position = image_rect.move(0, height)
+        self.speed = speed
+
+        #Starting position
+        self.rect.y = int(0.9 * screenheight)
+        self.rect.x = screenwidth // 2 - self.rect.width // 2
+
+        self.hyperbombs = 0
+        self.hyperbeams = 0
+        self.shotType = "basic"
+
+    def move_left(self, modifier):
+        self.rect.x -= self.speed * modifier
+
+    def move_right(self, modifier):
+        self.rect.x += self.speed * modifier
+
+    def move_up(self, modifier):
+        self.rect.y -= self.speed * modifier
+
+    def move_down(self, modifier):
+        self.rect.y += self.speed * modifier
+    
+    def shoot(self):
+        print("blam!")
